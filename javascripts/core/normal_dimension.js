d7208506adcf3365d4698e42f4a8c84e9b24d149
Aarex Tiaokhiao <aarextiaokhiao@gmail.com>
2018-08-06 06:19:33 -0400

    Normal dimensions fix

--- javascripts/core/normal_dimension.js
+++ javascripts/core/normal_dimension.js
@@ -4,7 +4,7 @@ function getDimensionFinalMultiplier(tier) {
 
   let multiplier = new Decimal(player[name + 'Pow']);
 
-  if (player.currentEternityChall == "eterc11") return player.infinityPower.pow(7).max(1).times(getDimensionBoostPower().pow(player.resets - tier + 1).max(1))
+  if (player.currentEternityChall == "eterc11") return player.infinityPower.pow(7).max(1).times(Decimal.pow(getDimensionBoostPower(), player.resets - tier + 1).max(1))
   if (player.currentChallenge == "challenge7") {
       if (tier == 4) multiplier = multiplier.pow(1.4)
       if (tier == 2) multiplier = multiplier.pow(1.7)
@@ -88,7 +88,7 @@ function getDimensionFinalMultiplier(tier) {
 
 function getDimensionDescription(tier) {
   var name = TIER_NAMES[tier];
-  if (tier > Math.min(player.currentEternityChall == "eterc3" ? 3 : (player.currentChallenge == "challenge4" || player.currentChallenge == "postc1") ? 5 : 7, player.resets + 3)) return getFullExpansion(tier > 7 && player.currentChallenge == "challenge11" ? player[name + "Amount"].round() : player[name + 'Bought']) + ' (' + dimBought(tier) + ')';
+  if (tier > Math.min(player.currentEternityChall == "eterc3" ? 3 : (player.currentChallenge == "challenge4" || player.currentChallenge == "postc1") ? 5 : 7, player.resets + 3)) return getFullExpansion(tier > 7 && player.currentChallenge == "challenge11" ? Math.round(player[name + "Amount"].toNumber()) : player[name + 'Bought']) + ' (' + dimBought(tier) + ')';
   else return shortenDimensions(player[name + 'Amount']) + ' (' + dimBought(tier) + ')  (+' + formatValue(player.options.notation, getDimensionRateOfChange(tier), 2, 2) + '%/s)';
 }
 
