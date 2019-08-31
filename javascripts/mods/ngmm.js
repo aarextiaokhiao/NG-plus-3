67915acb3c6abdcdf6638fe69205671308d0636c
pg132 <42481851+pg132@users.noreply.github.com>
2019-08-30 21:31:14 -0700

    r137 makes dilated galaxies more effective in GP formula

--- javascripts/mods/ngmm.js
+++ javascripts/mods/ngmm.js
@@ -1,6 +1,7 @@
 function getGSAmount() {
 	if (isEmptiness) return new Decimal(0)
 	let galaxies = player.galaxies + player.replicanti.galaxies + player.dilation.freeGalaxies;
+	if (player.achievements.includes("r137")) galaxies += Math.max(200,player.dilation.freeGalaxies*3) + player.dilation.freeGalaxies
 	let y = 1.5 
 	if (player.challenges.includes("postcngmm_1")) {
 		y += Math.max(0, 0.05*(galaxies - 10)) + 0.005 * Math.pow(Math.max(0, galaxies-30) , 2)
