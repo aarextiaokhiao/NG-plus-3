8f62d19d15f9889228d3394c28b5a85fd4e3e90f
Aarex Tiaokhiao <aarextiaokhiao@gmail.com>
2020-05-31 22:54:57 -0400

    v2.21 Beta A1 Alpha 13.03 Patch 3

--- javascripts/core/newsticker.js
+++ javascripts/core/newsticker.js
@@ -331,8 +331,9 @@ newsArray = [//always true
 ["You are a no-life news reader.", "player.newsArray.length >= 400", "am178"],
 ["Is not called matter dimensions for that?", "tmp.ngp3 && tmp.qu.bigRip.active", "am179"],
 ["aarex has retired back to his cave after apeasing the masses with the higgs boson", false, "am180"],
-["We launched a mission rocket to land on a ghostly moon.", false, "am181"]
-/*NEXT ID: am182*/
+["We launched a mission rocket to land on a ghostly moon.", false, "am181"],
+["BREAKING NEWS! After the killing of a antimatter person, antimatter people started protesting!", false, "am182"]
+/*NEXT ID: am183*/
 ];}
 
 document.addEventListener("visibilitychange", function() {if (!document.hidden) {scrollNextMessage();}}, false);
