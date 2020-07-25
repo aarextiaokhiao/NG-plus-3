506a8a0523f58ae810be4455ff6812d2f72d2800
pg132 <42481851+pg132@users.noreply.github.com>
2020-07-24 19:22:50 -0700

    fix

--- javascripts/mods/ngp3/quarks_gluons.js
+++ javascripts/mods/ngp3/quarks_gluons.js
@@ -288,7 +288,7 @@ function maxUpgradeColorDimPower(){
 	if (!quarks.gte(currCost)) return
 	var log105 = Math.log10(5)
 	
-	var tobuy = Math.floor(quarks.times(4).plus(currCost).log10()/log105-1)
+	var tobuy = Math.floor(quarks.times(4).plus(currCost).div(currCost).log10()/log105-1)
 	//log_5(4*totalquarks+currentcost)-1
 	var costToBuy = Decimal.pow(5,tobuy).minus(1).div(4).times(currCost).min(Decimal.pow(2,1024)) 
 	//only costs quarks if you have less than e308 of them
