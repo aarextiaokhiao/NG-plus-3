49bfe9c64aad59c923e4a37f8772271fc52f3903
pg132 <42481851+pg132@users.noreply.github.com>
2020-08-13 15:33:52 -0700

    clean up startChallenge function

--- javascripts/game.js
+++ javascripts/game.js
@@ -6686,7 +6686,7 @@ function eternity(force, auto, presetLoad, dilated) {
         }
         Marathon2 = 0;
         doAutoEterTick()
-        if (tmp.ngp3&&player.dilation.upgrades.includes("ngpp3")&&getEternitied()>=1e9) player.dbPower=new Decimal(1)
+        if (tmp.ngp3 && player.dilation.upgrades.includes("ngpp3") && getEternitied() >= 1e9) player.dbPower=new Decimal(1)
         if (tmp.ngp3) updateBreakEternity()
     }
 }
@@ -6696,15 +6696,15 @@ function resetReplicantiUpgrades() {
 	player.replicanti.chance = keepPartial ? Math.min(player.replicanti.chance, 1) : 0.01
 	player.replicanti.interval = keepPartial ? Math.max(player.replicanti.interval, player.timestudy.studies.includes(22) ? 1 : 50) : 1000
 	player.replicanti.gal = 0
-	player.replicanti.chanceCost = Decimal.pow(1e15, player.replicanti.chance * 100).times(player.galacticSacrifice!==undefined?1e75:1e135)
-	player.replicanti.intervalCost = Decimal.pow(1e10, Math.round(Math.log10(1000/player.replicanti.interval)/-Math.log10(0.9))).times(player.galacticSacrifice!==undefined?1e80:player.boughtDims?1e150:1e140)
-	player.replicanti.galCost = new Decimal(player.galacticSacrifice!=undefined?1e110:1e170)	
+	player.replicanti.chanceCost = Decimal.pow(1e15, player.replicanti.chance * 100).times(player.galacticSacrifice !== undefined ? 1e75 : 1e135)
+	player.replicanti.intervalCost = Decimal.pow(1e10, Math.round(Math.log10(1000 / player.replicanti.interval) / -Math.log10(0.9))).times(player.galacticSacrifice !== undefined ? 1e80 : player.boughtDims ? 1e150 : 1e140)
+	player.replicanti.galCost = new Decimal(player.galacticSacrifice != undefined ? 1e110 : 1e170)	
 }
 
 function challengesCompletedOnEternity(bigRip) {
 	var array = []
-	if (getEternitied()>1||bigRip||player.achievements.includes("ng3p51")) for (i=1;i<(player.galacticSacrifice?15:13);i++) array.push("challenge"+i)
-	if (player.achievements.includes("r133")) for (i=0;i<order.length;i++) array.push(order[i])
+	if (getEternitied() > 1 || bigRip || player.achievements.includes("ng3p51")) for (i = 1; i < (player.galacticSacrifice ? 15 : 13); i++) array.push("challenge" + i)
+	if (player.achievements.includes("r133")) for (i = 0; i < order.length; i++) array.push(order[i])
 	return array
 }
 
@@ -6764,186 +6764,64 @@ function startChallenge(name) {
 	if (name.includes("post")) {
 		if (player.postChallUnlocked < checkICID(name)) return
 		var target = getGoal(name)
-		} else var target = new Decimal(Number.MAX_VALUE)
-		if (player.options.challConf && name != "") if (!confirm("You will start over with just your infinity upgrades, and achievements. You need to reach " + (name.includes("post") ? "a set goal" : "infinity") + " with special conditions. NOTE: The rightmost infinity upgrade column doesn't work on challenges.")) return
+	} else var target = new Decimal(Number.MAX_VALUE)
+	if (player.options.challConf && name != "") if (!confirm("You will start over with just your infinity upgrades, and achievements. You need to reach " + (name.includes("post") ? "a set goal" : "infinity") + " with special conditions. NOTE: The rightmost infinity upgrade column doesn't work on challenges.")) return
 	if (player.tickspeedBoosts !== undefined) player.tickspeedBoosts = 0
 	if (name == "postc1" && player.currentEternityChall != "" && inQC(4) && inQC(6)) giveAchievement("The Ultimate Challenge")
-	player = {
-		money: new Decimal(10),
-		tickSpeedCost: new Decimal(1000),
-		tickBoughtThisInf: resetTickBoughtThisInf(),
-		firstCost: new Decimal(10),
-        secondCost: new Decimal(100),
-        thirdCost: new Decimal(10000),
-        fourthCost: new Decimal(1000000),
-        fifthCost: new Decimal(1e9),
-        sixthCost: new Decimal(1e13),
-        seventhCost: new Decimal(1e18),
-        eightCost: new Decimal(1e24),
-        firstAmount: new Decimal(0),
-        secondAmount: new Decimal(0),
-        thirdAmount: new Decimal(0),
-        fourthAmount: new Decimal(0),
-        firstBought: 0,
-        secondBought: 0,
-        thirdBought: 0,
-        fourthBought: 0,
-        fifthAmount: new Decimal(0),
-        sixthAmount: new Decimal(0),
-        seventhAmount: new Decimal(0),
-        eightAmount: new Decimal(0),
-        fifthBought: 0,
-        sixthBought: 0,
-        seventhBought: 0,
-        eightBought: 0,
-        firstPow: new Decimal(1),
-        secondPow: new Decimal(1),
-        thirdPow: new Decimal(1),
-        fourthPow: new Decimal(1),
-        fifthPow: new Decimal(1),
-        sixthPow: new Decimal(1),
-        seventhPow: new Decimal(1),
-        eightPow: new Decimal(1),
-        boughtDims: player.boughtDims,
-        totalBoughtDims: resetTotalBought(),
-        sacrificed: new Decimal(0),
-		thisInfinityTime: 0,
-		resets: 0,
-		galaxies: 0,
-		interval: null,
-		galacticSacrifice: newGalacticDataOnInfinity(),
-		costMultipliers: [new Decimal(1e3), new Decimal(1e4), new Decimal(1e5), new Decimal(1e6), new Decimal(1e8), new Decimal(1e10), new Decimal(1e12), new Decimal(1e15)],
-		tickspeedMultiplier: new Decimal(10),
-		chall2Pow: 1,
-		chall3Pow: new Decimal(0.01),
-		matter: new Decimal(0),
-		achievements: player.achievements,
-		chall11Pow: new Decimal(1),
-		postC4Tier: 1,
-		postC8Mult: new Decimal(1),
-		//above is everything that is changed
-      challenges: player.challenges,
-      currentChallenge: name,
-      infinityUpgrades: player.infinityUpgrades,
-      infinityUpgradesRespecced: player.infinityUpgradesRespecced,
-      setsUnlocked: player.setsUnlocked,
-      infinityPoints: player.infinityPoints,
-      infinitied: player.infinitied,
-      infinitiedBank: player.infinitiedBank,
-      totalTimePlayed: player.totalTimePlayed,
-      bestInfinityTime: player.bestInfinityTime,
-      dbPower: player.dbPower,
-      tdBoosts: player.tdBoosts,
-      tickspeedBoosts: player.tickspeedBoosts,
-      pSac: player.pSac,
-      totalmoney: player.totalmoney,
-      lastUpdate: player.lastUpdate,
-      achPow: player.achPow,
-      autobuyers: player.autobuyers,
-      
-      newsArray: player.newsArray,
-      
-      partInfinityPoint: player.partInfinityPoint,
-      partInfinitied: player.partInfinitied,
-      break: player.break,
-      challengeTimes: player.challengeTimes,
-      infchallengeTimes: player.infchallengeTimes,
-      lastTenRuns: player.lastTenRuns,
-      lastTenEternities: player.lastTenEternities,
-      infMult: player.infMult,
-      infMultCost: player.infMultCost,
-      tickSpeedMultDecrease: player.tickSpeedMultDecrease,
-      tickSpeedMultDecreaseCost: player.tickSpeedMultDecreaseCost,
-      dimensionMultDecrease: player.dimensionMultDecrease,
-      dimensionMultDecreaseCost: player.dimensionMultDecreaseCost,
-      extraDimPowerIncrease: player.extraDimPowerIncrease,
-      dimPowerIncreaseCost: player.dimPowerIncreaseCost,
-      version: player.version,
-      postChallUnlocked: player.postChallUnlocked,
-      
-      overXGalaxies: player.overXGalaxies,
-      overXGalaxiesTickspeedBoost: player.overXGalaxiesTickspeedBoost,
-      singularity: player.singularity,
-      dimtechs: player.dimtechs,
-      spreadingCancer: player.spreadingCancer,
-      infDimensionsUnlocked: player.infDimensionsUnlocked,
-      infinityPower: player.infinityPower,
-      infinityDimension1: player.infinityDimension1,
-      infinityDimension2: player.infinityDimension2,
-      infinityDimension3: player.infinityDimension3,
-      infinityDimension4: player.infinityDimension4,
-      infinityDimension5: player.infinityDimension5,
-      infinityDimension6: player.infinityDimension6,
-      infinityDimension7: player.infinityDimension7,
-      infinityDimension8: player.infinityDimension8,
-      infDimBuyers: player.infDimBuyers,
-      timeShards: player.timeShards,
-      tickThreshold: player.tickThreshold,
-      timeDimension1: player.timeDimension1,
-      timeDimension2: player.timeDimension2,
-      timeDimension3: player.timeDimension3,
-      timeDimension4: player.timeDimension4,
-      timeDimension5: player.timeDimension5,
-      timeDimension6: player.timeDimension6,
-      timeDimension7: player.timeDimension7,
-      timeDimension8: player.timeDimension8,
-      eternityPoints: player.eternityPoints,
-      eternities: player.eternities,
-      eternitiesBank: player.eternitiesBank,
-      thisEternity: player.thisEternity,
-      bestEternity: player.bestEternity,
-      eternityUpgrades: player.eternityUpgrades,
-      epmult: player.epmult,
-      epmultCost: player.epmultCost,
-      totalTickGained: player.totalTickGained,
-      offlineProd: player.offlineProd,
-      offlineProdCost: player.offlineProdCost,
-      challengeTarget: target,
-      autoSacrifice: player.autoSacrifice,
-      replicanti: player.replicanti,
-      timestudy: player.timestudy,
-      eternityChalls: player.eternityChalls,
-      eternityChallGoal: player.eternityChallGoal,
-      currentEternityChall: player.currentEternityChall,
-      eternityChallUnlocked: player.eternityChallUnlocked,
-      etercreq: player.etercreq,
-      autoIP: player.autoIP,
-      autoTime: player.autoTime,
-      infMultBuyer: player.infMultBuyer,
-      autoCrunchMode: player.autoCrunchMode,
-      autoEterMode: player.autoEterMode,
-      peakSpent: player.peakSpent,
-      respec: player.respec,
-      respecMastery: player.respecMastery,
-      eternityBuyer: player.eternityBuyer,
-      eterc8ids: player.eterc8ids,
-      eterc8repl: player.eterc8repl,
-      dimlife: player.dimlife,
-      dead: player.dead,
-      dilation: player.dilation,
-      exdilation: player.exdilation,
-      blackhole: player.blackhole,
-      blackholeDimension1: player.blackholeDimension1,
-      blackholeDimension2: player.blackholeDimension2,
-      blackholeDimension3: player.blackholeDimension3,
-      blackholeDimension4: player.blackholeDimension4,
-      blackholeDimension5: player.blackholeDimension5,
-      blackholeDimension6: player.blackholeDimension6,
-      blackholeDimension7: player.blackholeDimension7,
-      blackholeDimension8: player.blackholeDimension8,
-      why: player.why,
-      shameLevel: player.shameLevel,
-      options: player.options,
-      meta: player.meta,
-      masterystudies: player.masterystudies,
-      autoEterOptions: player.autoEterOptions,
-      galaxyMaxBulk: player.galaxyMaxBulk,
-      quantum: tmp.qu,
-      old: player.old,
-      dontWant: player.dontWant,
-      ghostify: player.ghostify,
-      aarexModifications: player.aarexModifications
-    };
+	
+	player.money = new Decimal(10)
+	player.tickSpeedCost = new Decimal(1000)
+	player.tickBoughtThisInf = resetTickBoughtThisInf()
+	player.firstCost = new Decimal(10)
+        player.secondCost = new Decimal(100)
+        player.thirdCost = new Decimal(10000)
+        player.fourthCost = new Decimal(1000000)
+        player.fifthCost = new Decimal(1e9)
+        player.sixthCost = new Decimal(1e13)
+        player.seventhCost = new Decimal(1e18)
+        player.eightCost = new Decimal(1e24)
+        player.firstAmount = new Decimal(0)
+        player.secondAmount = new Decimal(0)
+        player.thirdAmount = new Decimal(0)
+        player.fourthAmount = new Decimal(0)
+        player.firstBought = 0
+        player.secondBought = 0
+        player.thirdBought = 0
+        player.fourthBought = 0
+        player.fifthAmount = new Decimal(0)
+        player.sixthAmount = new Decimal(0)
+        player.seventhAmount = new Decimal(0)
+        player.eightAmount = new Decimal(0)
+        player.fifthBought = 0
+        player.sixthBought = 0
+        player.seventhBought = 0
+        player.eightBought = 0
+        player.firstPow = new Decimal(1)
+        player.secondPow = new Decimal(1)
+        player.thirdPow = new Decimal(1)
+        player.fourthPow = new Decimal(1)
+        player.fifthPow = new Decimal(1)
+        player.sixthPow = new Decimal(1)
+        player.seventhPow = new Decimal(1)
+        player.eightPow = new Decimal(1)
+        player.boughtDims = player.boughtDims
+        player.totalBoughtDims = resetTotalBought()
+        player.sacrificed = new Decimal(0)
+	player.thisInfinityTime = 0
+	player.resets = 0
+	player.galaxies = 0
+	player.interval = null
+	player.galacticSacrifice = newGalacticDataOnInfinity()
+	player.costMultipliers = [new Decimal(1e3), new Decimal(1e4), new Decimal(1e5), new Decimal(1e6), new Decimal(1e8), new Decimal(1e10), new Decimal(1e12), new Decimal(1e15)]
+	player.tickspeedMultiplier= new Decimal(10)
+	player.chall2Pow = 1
+	player.chall3Pow = new Decimal(0.01)
+	player.matter = new Decimal(0)
+	player.achievements = player.achievements
+	player.chall11Pow = new Decimal(1)
+	player.postC4Tier = 1
+	player.postC8Mult = new Decimal(1)
+    
 	if (inNC(10) || player.currentChallenge == "postc1") {
 		player.thirdCost = new Decimal(100)
 		player.fourthCost = new Decimal(500)
@@ -6956,7 +6834,7 @@ function startChallenge(name) {
 	resetPSac()
 	resetTDs()
 	reduceDimCosts()
-	if (player.currentChallenge == "postc1") player.costMultipliers = [new Decimal(1e3),new Decimal(5e3),new Decimal(1e4),new Decimal(1.2e4),new Decimal(1.8e4),new Decimal(2.6e4),new Decimal(3.2e4),new Decimal(4.2e4)];
+	if (player.currentChallenge == "postc1") player.costMultipliers = [new Decimal(1e3), new Decimal(5e3), new Decimal(1e4), new Decimal(1.2e4), new Decimal(1.8e4), new Decimal(2.6e4), new Decimal(3.2e4), new Decimal(4.2e4)];
 	if (player.currentChallenge == "postc2") {
 		player.eightAmount = new Decimal(1);
 		player.eightBought = 1;
@@ -6987,7 +6865,7 @@ function startChallenge(name) {
 	if (player.achievements.includes("r36")) player.tickspeed = player.tickspeed.times(0.98);
 	if (player.achievements.includes("r45")) player.tickspeed = player.tickspeed.times(0.98);
 	if (player.achievements.includes("r66")) player.tickspeed = player.tickspeed.times(0.98);
-	if (player.achievements.includes("r83")) player.tickspeed = player.tickspeed.times(Decimal.pow(0.95,player.galaxies));
+	if (player.achievements.includes("r83")) player.tickspeed = player.tickspeed.times(Decimal.pow(0.95, player.galaxies));
 
 	showTab('dimensions')
 	updateChallenges()
