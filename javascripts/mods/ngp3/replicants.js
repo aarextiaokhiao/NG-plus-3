01f69b31310c7bed68606b18bdcd14ef797eeb03
pg132 <picogilman@gmail.com>
2020-08-23 21:05:03 -0700

    Beta 0 Alpha 7.3
    replicanti display wrong speed, cleaning up

--- javascripts/mods/ngp3/replicants.js
+++ javascripts/mods/ngp3/replicants.js
@@ -1,21 +1,5 @@
-function updateReplicantsTab(){
-	document.getElementById("replicantiAmount2").textContent = shortenDimensions(player.replicanti.amount)
-	document.getElementById("replicantReset").className = player.replicanti.amount.lt(tmp.qu.replicants.requirement) ? "unavailablebtn" : "storebtn"
-	document.getElementById("replicantReset").innerHTML = "Reset replicanti amount for a replicant.<br>(requires " + shortenCosts(tmp.qu.replicants.requirement) + " replicanti)"
-	document.getElementById("replicantAmount").textContent = shortenDimensions(tmp.qu.replicants.amount)
-	document.getElementById("workerReplAmount").textContent = shortenDimensions(tmp.twr)
-	document.getElementById("babyReplAmount").textContent = shortenDimensions(tmp.qu.replicants.babies)
-
-	var gatherRateData = getGatherRate()
-	document.getElementById("normalReplGatherRate").textContent = shortenDimensions(gatherRateData.normal)
-	document.getElementById("workerReplGatherRate").textContent = shortenDimensions(gatherRateData.workersTotal)
-	document.getElementById("babyReplGatherRate").textContent = shortenDimensions(gatherRateData.babies)
-	document.getElementById("gatherRate").textContent = tmp.qu.nanofield.producingCharge ? '-' + shortenDimensions(getQuarkLossProduction()) + '/s' : '+' + shortenDimensions(gatherRateData.total) + '/s'
-
-	document.getElementById("gatheredQuarks").textContent = shortenDimensions(tmp.qu.replicants.quarks.floor())
-	document.getElementById("quarkTranslation").textContent = getFullExpansion(Math.round(tmp.pe * 100))
-
-	var eggonRate = tmp.twr.times(getEmperorDimensionMultiplier(1)).times(3)
+function babyRateUpdating(){
+	var eggonRate = tmp.twr.times(getEmperorDimensionMultiplier(1)).times(3).div((player.achievements.includes("ng3p35")) ? 1 : 10).times(getSpinToReplicantiSpeed())
 	if (eggonRate.lt(30)) {
 		document.getElementById("eggonRate").textContent = shortenDimensions(eggonRate)
 		document.getElementById("eggonRateTimeframe").textContent = "minute"
@@ -23,9 +7,17 @@ function updateReplicantsTab(){
 		document.getElementById("eggonRate").textContent = shortenMoney(eggonRate.div(60))
 		document.getElementById("eggonRateTimeframe").textContent = "second"
 	}
-	document.getElementById("feedNormal").className = (canFeedReplicant(1) ? "stor" : "unavailabl") + "ebtn"
-	document.getElementById("workerProgress").textContent = Math.round(tmp.eds[1].progress.toNumber() * 100) + "%"
+}
 
+function preonGatherRateUpdating(){
+	var gatherRateData = getGatherRate()
+	document.getElementById("normalReplGatherRate").textContent = shortenDimensions(gatherRateData.normal)
+	document.getElementById("workerReplGatherRate").textContent = shortenDimensions(gatherRateData.workersTotal)
+	document.getElementById("babyReplGatherRate").textContent = shortenDimensions(gatherRateData.babies)
+	document.getElementById("gatherRate").textContent = tmp.qu.nanofield.producingCharge ? '-' + shortenDimensions(getQuarkLossProduction()) + '/s' : '+' + shortenDimensions(gatherRateData.total) + '/s'
+}
+
+function growupRateUpdating(){
 	if (!hasNU(2)) {
 		document.getElementById("eggonAmount").textContent = shortenDimensions(tmp.qu.replicants.eggons)
 		document.getElementById("hatchProgress").textContent = Math.round(tmp.qu.replicants.babyProgress.toNumber() * 100)+"%"
@@ -40,6 +32,26 @@ function updateReplicantsTab(){
 		document.getElementById("growupRateUnit").textContent = "second"
 	}
 	document.getElementById("growupProgress").textContent = Math.round(tmp.qu.replicants.ageProgress.toNumber() * 100) + "%"
+}
+
+function updateReplicantsTab(){
+	document.getElementById("replicantiAmount2").textContent = shortenDimensions(player.replicanti.amount)
+	document.getElementById("replicantReset").className = player.replicanti.amount.lt(tmp.qu.replicants.requirement) ? "unavailablebtn" : "storebtn"
+	document.getElementById("replicantReset").innerHTML = "Reset replicanti amount for a replicant.<br>(requires " + shortenCosts(tmp.qu.replicants.requirement) + " replicanti)"
+	document.getElementById("replicantAmount").textContent = shortenDimensions(tmp.qu.replicants.amount)
+	document.getElementById("workerReplAmount").textContent = shortenDimensions(tmp.twr)
+	document.getElementById("babyReplAmount").textContent = shortenDimensions(tmp.qu.replicants.babies)
+
+	preonGatherRateUpdating()
+
+	document.getElementById("gatheredQuarks").textContent = shortenDimensions(tmp.qu.replicants.quarks.floor())
+	document.getElementById("quarkTranslation").textContent = getFullExpansion(Math.round(tmp.pe * 100))
+
+	babyRateUpdating()
+	document.getElementById("feedNormal").className = (canFeedReplicant(1) ? "stor" : "unavailabl") + "ebtn"
+	document.getElementById("workerProgress").textContent = Math.round(tmp.eds[1].progress.toNumber() * 100) + "%"
+
+	growupRateUpdating()
 	
 	document.getElementById("reduceHatchSpeed").innerHTML = "Hatch speed: " + hatchSpeedDisplay() + " -> " + hatchSpeedDisplay(true) + "<br>Cost: " + shortenDimensions(tmp.qu.replicants.hatchSpeedCost) + " for all 3 gluons"
 	if (player.ghostify.milestones > 7) updateReplicants("display")
