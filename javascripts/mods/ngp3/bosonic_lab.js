7d0bd3164ea79cd15edeb8100c4b8b2f42284bb6
Matty Jones <mlj@kount.com>
2020-09-21 11:13:36 -0600

    Updated wQkProgress to be positive and negative instead of up and down

--- javascripts/mods/ngp3/bosonic_lab.js
+++ javascripts/mods/ngp3/bosonic_lab.js
@@ -916,7 +916,7 @@ function updateWZBosonsTab() {
 	document.getElementById("apProduction").textContent = (data3.dPUse ? "-" : "+") + shorten(r) + "/s"
 	document.getElementById("apUse").textContent = data3.dPUse == 0 ? "" : "You are currently consuming Anti-Preons to " + (["", "decay W Bosons", "oscillate Z Neutrinos", "convert W- to W+ Bosons"])[data3.dPUse] + "."
 	document.getElementById("wQkType").textContent = data3.wQkUp ? "positive" : "negative"
-	document.getElementById("wQkProgress").textContent = data3.wQkProgress.times(100).toFixed(1) + "% to turn W Boson to a" + (data3.wQkUp ? " down" : "n up")+" Boson."
+	document.getElementById("wQkProgress").textContent = data3.wQkProgress.times(100).toFixed(1) + "% to turn W Boson to a" + (data3.wQkUp ? " negative" : " positive")+" Boson."
 	document.getElementById("wQk").className = show0 ? "zero" : data3.wQkUp ? "up" : "down"
 	document.getElementById("wQkSymbol").textContent = show0 ? "0" : data3.wQkUp ? "+" : "−"
 	document.getElementById("wpb").textContent = shortenDimensions(data3.wpb)
