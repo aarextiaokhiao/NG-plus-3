42f90c0427c274a550520c55192709d17dc6a64e
aarextiaokhiao <aarextiaokhiao@gmail.com>
2023-07-14 23:28:06 -0400

    v2.31: NGUdS': 7/14/23

--- javascripts/mods/ngud/update.js
+++ javascripts/mods/ngud/update.js
@@ -239,7 +239,7 @@ function hasExdilation() {
 }
 
 function getExdilationReq() {
-	if (mod.udsp) return {ep: "1e10000", dt: 1e27}
+	if (mod.udsp) return {ep: "1e45000", dt: 1e27}
 	return {ep: "1e10000", dt: 1e30}
 }
 
