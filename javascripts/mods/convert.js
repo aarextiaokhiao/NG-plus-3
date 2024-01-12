1ce4a1e7012cf376d8f97e86a1789229f508a7ce
aarextiaokhiao <aarextiaokhiao@gmail.com>
2024-01-11 23:44:51 -0500

    v2.31: 1/11/24 Patch
    Beautify the code

--- javascripts/mods/convert.js
+++ javascripts/mods/convert.js
@@ -13,12 +13,13 @@ function updateConvertSave(convertMod) {
 }
 
 function eligibleConvert() {
-	if (mod.ngpp == 2 && !player.dilation.studies.includes(1)) convert = "NGUdS'"
-	if (!mod.ngp3 && !inEasierMode() && !aarMod.ersVersion) {
+	if (!inEasierMode() || mod.rs) return
+	if (mod.ngp3) {
+		if (!player.dilation.studies.includes(1)) return "NGUdS'"
+	} else {
 		if (mod.ngud) convert = "NGUdS'"
 		else convert = "NG+3"
-	} else convert = undefined
-	return convert
+	}
 }
 
 function convertSave(conversion) {
