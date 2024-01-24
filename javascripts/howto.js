47cb8026c090ae98087f0f67d518f66cda515a54
aarextiaokhiao <aarextiaokhiao@gmail.com>
2024-01-23 23:45:15 -0500

    v2.31: 1/23/24 Rephrase

--- javascripts/howto.js
+++ javascripts/howto.js
@@ -418,7 +418,7 @@ let HOW_TO = [
 		tab: "bl",
 		layer: "fundament",
 		desc: `<b>Hypotheses</b><br>
-		You have a 5x5 grid where you must place down a pair of Hypotheses. A pair boosts things depending on Hypotheses you placed.
+		You have a 5x5 grid where you can place down Hypotheses. A pair of adjacent Hypotheses boosts things depending on their types.
 		<br><br>
 		<b>Bosonic Milestones</b><br>
 		At a specific amount of best Bosonic Matter, you'll unlock a Bosonic Milestone. Best Bosonic Matter strengthens some milestones.
