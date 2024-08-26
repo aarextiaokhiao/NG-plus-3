ca1f1200f877ad244347863b3d69420f27596c14
aarextiaokhiao <aarextiaokhiao@gmail.com>
2024-08-25 21:57:28 -0400

    Update howto.js

--- javascripts/howto.js
+++ javascripts/howto.js
@@ -419,7 +419,7 @@ let HOW_TO = [
 		title: "Neutrinos",
 		layer: "fundament",
 		desc: `There are 3 kinds of Neutrinos: Electron, Mu, Tau. You gain Neutrinos of a single kind per 1 Antimatter Galaxy.<br>
-		Going Quantum switches which Neutrino kind to produce. Fundamenting resets Neutrinos.
+		On going Quantum, you'll generate a different kind of Neutrinos. However, Fundamenting resets all Neutrinos.
 		<br><br>
 		You can spend Spectral Particles to boost Neutrinos.<br>
 		Neutrinos are used to boost things dependent on 3 types, and can be spent for upgrades or a repeatable which gives 3x SP per purchase.`
