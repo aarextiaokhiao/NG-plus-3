02ea8682ce9ae25c978e2a5ed6d1185d9679a4cc
aarextiaokhiao <aarextiaokhiao@gmail.com>
2024-09-30 23:52:42 -0400

    Update howto.js

--- javascripts/howto.js
+++ javascripts/howto.js
@@ -462,7 +462,7 @@ let HOW_TO = [
 			You can place down W+, W-, and Z Chargers on a 7x7 grid.<br>
 			W+ increases strength to adjacent Chargers but W- decreases strength.<br>
 			Chargers with positive strength only provide positive hypercharge while ones with negative strength provide negative hypercharge.<br>
-			Hypercharges are separate resources that do not add and generate each other. Instead, they provide milestones that are independent of these.
+			Hypercharges are separate resources that do not add and generate each other. Instead, they provide milestones that are independent of these. Some will boost the production of Bosons, so make sure to have presets for different buildups!
 		`
 	}, {
 		mod: _ => ngp3,
