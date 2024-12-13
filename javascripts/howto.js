ae52fa0ab594cf8bedd4e625a6e24152b17629e9
aarextiaokhiao <aarextiaokhiao@gmail.com>
2024-12-12 23:52:23 -0500

    Update howto.js

--- javascripts/howto.js
+++ javascripts/howto.js
@@ -381,10 +381,10 @@ let HOW_TO = [
 		title: "Decay",
 		layer: "quantum",
 		desc() {
-			let msg = `You can turn one color of Colored Quarks into Free Pilons, which gradually decay exponentially while generating Preonic Spin. Preonic Spins can be spent on a big set of upgrades.`
+			let msg = `You can turn one color of Colored Quarks into Free Preons, which gradually decay exponentially while generating Preonic Spin. Preonic Spins can be spent on a big set of upgrades.`
 
 			if (ghSave?.times || SPOILERS) msg += `<b>Radioactive Decays</b><br>
-				When you have enough Free Pilons, you will be able to sacrifice some upgrades in order to provide some boosts. However, each Decay nerfs Decay severally except the upgrades on the bottom!`
+				When you have enough Free Preons, you will be able to sacrifice some upgrades in order to provide some boosts. However, each Decay nerfs Decay severally except the upgrades on the bottom!`
 			else msg += `<b>Go Fundament to unlock a subfeature.</b>`
 
 			return msg
