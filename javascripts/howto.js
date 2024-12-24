997f8225c61957e6e9575b875834cd5b3b58ee8d
aarextiaokhiao <aarextiaokhiao@gmail.com>
2024-12-23 23:50:22 -0500

    Update howto.js

--- javascripts/howto.js
+++ javascripts/howto.js
@@ -486,7 +486,7 @@ let HOW_TO = [
 			W+ Chargers add the strength to adjacent chargers, but W- Chargers decrease the strength to adjacent chargers.<br>
 			Chargers with positive strength provide positive hypercharge, but chargers with negative strength provide negative hypercharge.<br><br>
 
-			Hypercharges are separate resources that don't boost each other. Instead, each provide milestones that are independent to hypercharges.<br>
+			Hypercharges are separate quantities that don't boost each other. Instead, each hypercharge provide milestones that are independent to both hypercharges.<br>
 			Some milestones boost Bosons, so make sure to setup presets! You definitely need at least two to progress.
 		`
 	}, {
