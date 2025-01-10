dd46a66d0f675ea8025099bf77ed7b22784978bc
aarextiaokhiao <aarextiaokhiao@gmail.com>
2025-01-09 23:53:42 -0500

    Update howto.js

--- javascripts/howto.js
+++ javascripts/howto.js
@@ -383,8 +383,8 @@ let HOW_TO = [
 		layer: "quantum",
 		desc: `Nanofield allows you to gain bonuses by getting pilon energy, produced by pilon charge. You can enable the production of pilon charge, which produces pilon energy, but also pilon anti-energy.<br>		
 		You can get rewards from your pilon energy, with the first one requiring 50 pilon energy.<br><br>
-		
-		Pilon anti-energy slows down pilon energy production until a cap of anti-energy is reached, where pilon energy production completely stops. When this happens, the only way to get more pilon energy is to get more pilon charge.<br><br>
+
+		Pilon anti-energy slows down pilon energy production until a cap of anti-energy is reached, where pilon energy production completely stops. When this happens, the only way to get more pilon energy is to get more pilon charge or go quantum.<br><br>
 		
 		Note: Pilon charge, energy, anti-energy, and power all reset on Quantum, but you keep whatever rewards you have already earned on previous Quantums.`
 	}, {
