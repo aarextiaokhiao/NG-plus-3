3c8c0af5ca7909f5dfa70ab0bf4bf8d0497c57ef
aarextiaokhiao <aarextiaokhiao@gmail.com>
2025-02-24 23:52:35 -0500

    Update howto.js

--- javascripts/howto.js
+++ javascripts/howto.js
@@ -485,7 +485,7 @@ let HOW_TO = [
 			You can place down W+, W-, and Z Chargers on a customizible 7x7 grid.
 			<br><br>
 
-			A W Charger provide strength onto chargers adjacent to it. W+ Chargers add strength, but W- Chargers decrease strength.<br>
+			A W Charger provide strength onto chargers adjacent to it. W+ Chargers add strength, but W- Chargers reduce strength.<br>
 			Chargers with positive strength provide positive hypercharge, but chargers with negative strength provide negative hypercharge.
 			<br><br>
 
