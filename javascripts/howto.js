6440c30f4830dfa134513b895c0b7daa6abe5097
aarextiaokhiao <aarextiaokhiao@gmail.com>
2025-03-03 23:54:45 -0500

    Update howto.js

--- javascripts/howto.js
+++ javascripts/howto.js
@@ -499,7 +499,7 @@ let HOW_TO = [
 		title: "Higgs Field",
 		layer: "fundament",
 		desc: `
-			At ??? Bosons, you can reset W & Z Bosons for Higgs. The first Higgs reset unlocks Higgs Field. Further Higgs resets require more Higgs. Unlike Dimensional Boosts and Galaxies, Higgs can be increased multiplely in one run.<br><br>
+			At ??? Bosons, you can reset W & Z Bosons for Higgs. The first Higgs reset unlocks Higgs Field. Further Higgs resets require more Higgs. Unlike Dimensional Boosts and Galaxies, Higgs can be increased by any amount in one run.<br><br>
 
 			Higgs Field has a grid of swappable boosts. You also produce Higgs Mass which strengthens all boosts from Higgs Field. Later rows provide weak effects at first, but gradually increase to be effective as earlier rows by gaining more Higgs Mass.
 		`,
