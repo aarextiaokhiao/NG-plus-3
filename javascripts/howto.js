67f54ae0b57c352c86cfee5387f4c3bf7d31572a
aarextiaokhiao <aarextiaokhiao@gmail.com>
2025-04-15 23:55:45 -0400

    Update howto.js

--- javascripts/howto.js
+++ javascripts/howto.js
@@ -492,7 +492,7 @@ let HOW_TO = [
 			<br><br>
 
 			Both hypercharges are separate quantities. Each type provides a set of milestones that are independent to those.<br>
-			Because milestones come with variety of effects, I recommend joining <a href="http://discord.gg/KsjcgskgTj" target="_newtab">the NG+3 Discord server</a> for further help and sharing presets.
+			Because all milestones provide with variety of effects, I recommend joining <a href="http://discord.gg/KsjcgskgTj" target="_newtab">the NG+3 Discord server</a> for further help and sharing presets.
 		`
 	}, {
 		mod: _ => ngp3,
