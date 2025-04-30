6a7a79e1e12feac5012c73e7b01cd87f9eba676d
aarextiaokhiao <aarextiaokhiao@gmail.com>
2025-04-29 23:55:37 -0400

    Update mastery_studies.js

--- javascripts/mods/ngp3/mastery_studies.js
+++ javascripts/mods/ngp3/mastery_studies.js
@@ -176,7 +176,7 @@ const MTS = MASTERY_STUDIES = {
 		331: "Dimension Supersonic scales 75% weaker and 240,000 later.",
 		332: "Antimatter Galaxies speed up Replicantis.",
 		341: "Pilons boost dilated time production at reduced rate.",
-		342: "All Replicated Galaxies boost equivalently.",
+		342: "All Replicated Galaxies are equally strong.",
 		343: "Tachyonic Galaxies are strong as normal Replicated Galaxies.",
 		344: "Pilons strengthen Replicated Galaxies.",
 		351: "Time Shards boost Meta Dimensions.",
