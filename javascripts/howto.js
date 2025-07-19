8c615431e2ce9adbb0aaa5c4f0e43b04d882cc54
aarextiaokhiao <aarextiaokhiao@gmail.com>
2025-07-18 23:56:32 -0400

    Update howto.js

--- javascripts/howto.js
+++ javascripts/howto.js
@@ -455,7 +455,7 @@ let HOW_TO = [
 			<br><br>
 
 			<b>Brave Milestones</b><br>
-			Brave Milestones can be achieved by going Fundament while going Quantums as least as you can. Like Speedrun Milestones, each Brave Milestone provides you with a reward that greatly boosts progress and makes Fundaments more efficient.
+			Brave Milestones can be achieved by going Fundament while going least Quantums as you can. Like Speedrun Milestones, each Brave Milestone provides you with a reward that greatly boosts progress and makes Fundaments more efficient.
 			<br><br>
 
 			<b>Automators</b><br>
