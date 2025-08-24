f7536be265882d05896b96bb0f46c9407544a959
aarextiaokhiao <aarextiaokhiao@gmail.com>
2025-08-23 23:56:40 -0400

    Update howto.js

--- javascripts/howto.js
+++ javascripts/howto.js
@@ -489,7 +489,7 @@ let HOW_TO = [
 			<br><br>
 
 			The idle part is where Photons are generated, based on various factors.<br>
-			Photons strengthen all 8 colors of Lights.<br>
+			Photons strengthen 8 colors of Lights.<br>
 			To get more colors, you must gain more Photons.<br>
 			Fundament resets all Photons and Lights.
 			<br><br>
