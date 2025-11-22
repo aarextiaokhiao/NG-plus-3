b6034d4a39e165ff7bd59a26b2cd659e82077ad5
aarextiaokhiao <aarextiaokhiao@gmail.com>
2025-11-21 23:57:19 -0500

    Update core.js

--- javascripts/mods/ngp3/core.js
+++ javascripts/mods/ngp3/core.js
@@ -345,7 +345,7 @@ function ngP3AchieveCheck() {
 	if (mod.ngp3) for (id = 0; id < player.masterystudies.length; id++) if (player.masterystudies[id].split("t")[1]) checkEmpty = false
 
 	let ableToGetRid2 = checkEmpty && player.dilation.active
-	let ableToGetRid3 = ableToGetRid2 && quSave.electrons.amount == 0	
+	let ableToGetRid3 = ableToGetRid2 && quSave.electrons.amount == 0
 	let ableToGetRid4 = ableToGetRid2 && inQC(2)
 	let ableToGetRid5 = ableToGetRid4 && player.dontWant
 	let ableToGetRid6 = ableToGetRid2 && inQC(6) && inQC(8)
