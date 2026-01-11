ad9d89cbf8ea73195d100a83895febc1f4c3edf1
aarextiaokhiao <aarextiaokhiao@gmail.com>
2026-01-10 23:56:55 -0500

    Update howto.js

--- javascripts/howto.js
+++ javascripts/howto.js
@@ -503,7 +503,7 @@ let HOW_TO = [
 
 		title: "W & Z Bosons",
 		layer: "fundament",
-		desc: `[ON REWORK, NO DESCRIPTION]
+		desc: `[ON DEVELOPMENT]
 			<br><br>
 
 			A idea has recently came up, which will be revealed soon.`
