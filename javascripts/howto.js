354719df165685868cc7a3ffb81085340bce8a3e
aarextiaokhiao <aarextiaokhiao@gmail.com>
2026-01-22 23:56:31 -0500

    Update howto.js

--- javascripts/howto.js
+++ javascripts/howto.js
@@ -505,7 +505,7 @@ let HOW_TO = [
 		layer: "fundament",
 		desc: `You start with 1 Z Bosons, which can be converted into W- Bosons for next Fundament run.<br>
 			W- Bosons provide nerfs at first, but can be cancelled out by having potential Z. On cancellation, you will start producing spare W+ Bosons which provide buffs.<br>
-			Z Bosons can be increased by attaining more Potential Z.`
+			Z Bosons can be increased by increasing your best-ever Potential Z.`
 	}, {
 		mod: _ => ngp3,
 		req: _ => ghSave?.hb?.unl,
