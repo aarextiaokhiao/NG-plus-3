f5fab051e1478d0acd68feeed59bd0fef00fb12b
aarextiaokhiao <aarextiaokhiao@gmail.com>
2026-02-18 23:55:55 -0500

    Update howto.js

--- javascripts/howto.js
+++ javascripts/howto.js
@@ -504,7 +504,7 @@ let HOW_TO = [
 		title: "W & Z Bosons",
 		layer: "fundament",
 		desc: `
-			You start with 1 Z Bosons. Z Bosons can be converted into W- Bosons for your next Fundament.
+			You start with 1 Z Boson. Z Bosons can be converted into W- Bosons for your next Fundament.
 			<br><br>
 
 			W- Bosons provide nerfs, but can be reduced by progression while gaining potential Z.<br>
