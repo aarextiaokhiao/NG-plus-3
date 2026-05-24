dc597efd64d5bfe99d0eb9ffb6971a56545a2523
aarextiaokhiao <aarextiaokhiao@gmail.com>
2026-05-23 22:34:35 -0400

    Update newsticker.js

--- javascripts/core/newsticker.js
+++ javascripts/core/newsticker.js
@@ -490,7 +490,7 @@ newsArray = [//always true
 ["How we suddenly got infinite ghosts?!", "ghostified && ghSave.ghostParticles.gte(Number.MAX_VALUE)", "am195"],
 ["The player has passed away. Fly high and godspeed.", "ghostified", "am197"],
 ["In the alternate timeline, we rather to go deeper to Fundament instead of transforming into a ghost.", "ghostified", "am202"],
-["Player, it seems that you have been here before. Or not... Whatever.", "aarMod.ngp4V != undefined", "am203"],
+["Player, it seems that you have been here before. Or not... Whatever.", "meta.save.rediscover.in != undefined", "am203"],
 ["ANTIMATTER DIMENSIONS // THE META CONTINUATION", true, "am214"],
 ["META-ANTIMATTER DIMENSIONS: DIMENSIONS OF LOWER DIMENSIONS", "mod.ngp3", "am234"],
 ["Before a blink of the eye, the man got physically obliterated by an antimatter explosion.", true, "am255"],
