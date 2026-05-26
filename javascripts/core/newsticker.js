8f5c62a558d52d2dbd3e1a4b23ab4e6f9906ed1e
aarextiaokhiao <aarextiaokhiao@gmail.com>
2026-05-25 22:08:22 -0400

    Update newsticker.js

--- javascripts/core/newsticker.js
+++ javascripts/core/newsticker.js
@@ -495,9 +495,9 @@ newsArray = [//always true
 ["META-ANTIMATTER DIMENSIONS: DIMENSIONS OF LOWER DIMENSIONS", "mod.ngp3", "am234"],
 ["Before a blink of the eye, the man got physically obliterated by an antimatter explosion.", true, "am255"],
 ["The winner is you!", "hasBadge('p3_end')", "am263"],
+["Grass got obliterated with antimatter grass. Seems like you couldn't made time to touch grass...", true, "am269"],
 
 /* PLACEHOLDER
-["[placeholder]", false, "am269"],
 ["[placeholder]", false, "am270"],
 ["[placeholder]", false, "am286"],
 ["[placeholder]", false, "am287"],
