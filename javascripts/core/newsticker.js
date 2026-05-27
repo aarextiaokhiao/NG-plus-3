f83c45ca94f4fea3749624fd81ccebd9f15c2b66
aarextiaokhiao <aarextiaokhiao@gmail.com>
2026-05-26 21:29:44 -0400

    Update newsticker.js

--- javascripts/core/newsticker.js
+++ javascripts/core/newsticker.js
@@ -610,7 +610,6 @@ function updateGhostlyNewsArray() {
 	["A new evidence states that mortal ghosts could exist through decay.", true, "gn26"],
 	["Ghosts have dominated some universes!", true, "gn27"],
 	["Boo! Kee-hee-hee!", "Math.random() <= 0.001", "gn28"],
-	["An anti-pilon ghost is discovered. He is evil to Nanofield. Don't ever observe him or else he will take over your Nanofield.", "hasAch('ng3p81')", "gn29"],
 	["Ghosts found a ghostly kind of Radioactive Decays!", "getRadioactiveDecays() >= 1", "gn30"],
 	["Ghosts discovered that some particles are ghostly!", true, "gn31"],
 	["Ghosts don't know how to write more news. They decided to stop it for today.", true, "gn32"],
@@ -666,6 +665,7 @@ function updateGhostlyNewsArray() {
 	["In a century, Aarex (the developer of NG+3) would be Ghosty.", true, "gn82"],
 
 	//['placeholder', false, "gn5"],
+	//['placeholder', false, "gn29"],
 	]
 }
 
