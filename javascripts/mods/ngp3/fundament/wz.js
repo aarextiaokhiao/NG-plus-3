be6276a70764c20a8a7f5eb7be0cb42fe46d3a1e
aarextiaokhiao <aarextiaokhiao@gmail.com>
2026-06-14 22:39:47 -0400

    v2.31: 6/14/26

--- javascripts/mods/ngp3/fundament/wz.js
+++ javascripts/mods/ngp3/fundament/wz.js
@@ -54,7 +54,7 @@ const WZ_BOSONS = {
 		pos: [
 			{ req: 0, eff: _ => x + 1, desc: x => `<b>${shorten(x)}x</b> Photons.` },
 			{ req: 1, eff: _ => x, desc: x => `Nanorewards scale <b>+${shorten(x)}x</b> later.` },
-			{ req: 1, eff: _ => x + 1, desc: x => `Replicantis slow down <b>${shorten(x)}x</b> slower.` },
+			{ req: 1, eff: _ => x / 10 + .9, desc: x => `Replicantis slow down <b>${shorten(x)}x</b> slower.` },
 		],
 		neg: [
 			{ req: 0, eff: _ => 1 / (x * 0.2 + 1), desc: x => `<b>^${shorten(x)}</b> Neutrinos.` },
