9aed9f2d662adf64f4c1546d1ee97fba2cb3bae2
aarextiaokhiao <aarextiaokhiao@gmail.com>
2026-06-15 22:32:03 -0400

    Update newsticker.js

--- javascripts/core/newsticker.js
+++ javascripts/core/newsticker.js
@@ -662,7 +662,7 @@ function updateGhostlyNewsArray() {
 	["If a ghost is completely invisible, would they have no sense of sight?", true, "gn79"],
 	["A paranormal event is currently happening. We are experiencing the intervention with ghosts and humans.", true, "gn80"],
 	["Once you are a ghost, you cannot resurrect back into your human soul.", true, "gn81"],
-	["In a century, Aarex (the developer of NG+3) would be Ghosty.", true, "gn82"],
+	["In half a century, Aarex (the developer of NG+3) would be Ghosty.", true, "gn82"],
 
 	//['placeholder', false, "gn24"],
 	//['placeholder', false, "gn29"],
