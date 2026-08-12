c1d282d332b0298b11fe08d5a212e5cdffde1fc5
aarextiaokhiao <aarextiaokhiao@gmail.com>
2026-08-11 22:17:11 -0400

    v2.31: 8/11/26 Newsticker Update

--- javascripts/core/newsticker.js
+++ javascripts/core/newsticker.js
@@ -495,7 +495,7 @@ newsArray = [//always true
 ["Due to annihilation between matter and antimatter, all grass got eradicated against antimatter grass. Seems like you couldn't made it on time.", true, "am269"],
 ["Developer of Ranks here...", true, "am270"],
 ["Matter is going down against the duplicant domination.", "hasAch('ng3p35')", "am77"],
-["In 1,752 AAD, the Nanofield solider ants declared war against Mattertopia's ant colony. One year later, Mattertopia won the battle.", "hasAch('ng3p35')", "am137"],
+["In 1,752 AAD, the Nanofield solider ants declared war against Mattertopia's ant colony. One big crunch later, Mattertopia won the battle.", "hasAch('ng3p35')", "am137"],
 ["7/27 WHEN YOU SEE ANTIMATTER", true, "am195"],
 ["By now, you might have Gamma Rays infested inside.", "PHOTON.unlocked() && ghSave.photons.amt.gte(1e100)", "am286"],
 
