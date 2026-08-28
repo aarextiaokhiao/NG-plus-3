53ca2a1d0b7e8b0faa4cf07bda5001c342e1f70b
aarextiaokhiao <aarextiaokhiao@gmail.com>
2026-08-27 22:37:26 -0400

    v2.31: 8/27/26 Newsticker Update
    Remove "am256."

--- javascripts/core/newsticker.js
+++ javascripts/core/newsticker.js
@@ -448,7 +448,6 @@ newsArray = [//always true
 ["Lightning loves NG+3! He even recommend World Restoration players with it! Have fun!", "mod.ngp3", "am252"],
 ["The year is 2028. Aarex has released Hadronize. But due to a lot of uproaring, NG+3 became unpopular. NG+3 will be likely to be at least dead. He has finally moved on.", true, "am253"],
 ["Antimatter Dimensions: NG+3! Now with 100% more balancing! + Less useless boosts! + Hyperexponential upgrades!", "mod.ngp3", "am254"],
-["According to the lore, we can confirm that Omnisity is going to be the last layer.", "mod.ngp3", "am256"],
 ["NG+3 Starter Pack: Creative mechanics, ideal dump, focuses on balancing a lot, a bit of development every day.", "mod.ngp3", "am257"],
 ["I bet you can't see the reference: An astronaut wearing the red suit.", true, "am258"],
 ['"Our world would be deadly as matter, if there are holes leading into matter cultists..." ~ Antimatter cultist', true, "am259"],
@@ -500,6 +499,7 @@ newsArray = [//always true
 ["By then, Gamma Rays would pass through your body.", "PHOTON.unlocked() && ghSave.photons.amt.gte(1e10)", "am286"],
 
 /* PLACEHOLDER
+["[placeholder]", false, "am256"],
 ["[placeholder]", false, "am287"],
 ["[placeholder]", false, "am288"],
 
