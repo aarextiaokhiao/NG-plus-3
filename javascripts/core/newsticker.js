0e95e66b945cb56b7d7d6dedf83106d87ee85d60
aarextiaokhiao <aarextiaokhiao@gmail.com>
2026-09-02 22:30:28 -0400

    v2.31: 9/2/26 Newsticker Update
    am285 removal.

--- javascripts/core/newsticker.js
+++ javascripts/core/newsticker.js
@@ -472,7 +472,6 @@ newsArray = [//always true
 ["~<: NEXTA COMMUNINC: CONSTELLA' THE STARS ~:> Stellarika Aarex's speaking to Sollings: The infamous \"Kibb's Point\" [<.>] has been disintegrated! aSAN~1,318,273^{*.*++} has been landed in release, featuring unseen methods extending to Kibb's Axioms! Be warned! Monstrous Xaliens pulled a nefarious abduct against aSAN~1,318,271! One is that -=>> is a numerous burst to (1,(((1,1,2),2)),2) arrays! Our megalo-arithmetic lab is working tediously for a drone evaluating their n...", "mod.ngp3", "am282"],
 ["It's 2023. Aarex managed to release an content update, but inflation seems to force him to reduce the formulas or underestimate the growth rate.", "mod.ngp3", "am283"],
 ["We are getting close to the true nature of Quantum. More to seem soon...", "ghostified", "am284"],
-["~<: NEXTA COMMUNINC: CONSTELLA' THE STARS ~:> It seems that Higgs Field are messing up our Nanofield...", "hasAch('ng3p91')", "am285"],
 ["Number hell is my enemy. ~ Aarex", "mod.ngp3", "am289"],
 ["Is NG+3 not the most influential mod of the pre-reality era? Of course the great Pelle would make a NG+3 reference!", "mod.ngp3", "am290"],
 
@@ -500,6 +499,7 @@ newsArray = [//always true
 
 /* PLACEHOLDER
 ["[placeholder]", false, "am256"],
+["[placeholder]", false, "am285"],
 ["[placeholder]", false, "am287"],
 ["[placeholder]", false, "am288"],
 
