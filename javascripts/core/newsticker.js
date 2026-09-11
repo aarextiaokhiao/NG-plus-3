c0a369c928dd48e110e5a7873ca647628bc92d00
aarextiaokhiao <aarextiaokhiao@gmail.com>
2026-09-10 22:47:46 -0400

    Update newsticker.js

--- javascripts/core/newsticker.js
+++ javascripts/core/newsticker.js
@@ -467,7 +467,7 @@ newsArray = [//always true
 ["In an alternate universe, @TruthNotScam9 convinced Aarex to add Scamify, leaving many sparks of outrage from the players. Both of them died soon afterward.", "mod.ngp3", "am281"],
 ["~<: NEXTA COMMUNINC: CONSTELLA' THE STARS ~:> Stellarika Aarex's speaking to Sollings: The infamous \"Kibb's Point\" [<.>] has been disintegrated! aSAN~1,318,273^{*.*++} has been landed in release, featuring unseen methods extending to Kibb's Axioms! Be warned! Monstrous Xaliens pulled a nefarious abduct against aSAN~1,318,271! One is that -=>> is a numerous burst to (1,(((1,1,2),2)),2) arrays! Our megalo-arithmetic lab is working tediously for a drone evaluating their n...", "mod.ngp3", "am282"],
 ["It's 2023. Aarex managed to release an content update, but inflation seems to force him to reduce the formulas or underestimate the growth rate.", "mod.ngp3", "am283"],
-["We are getting close to the true nature of Quantum. More to seem soon...", "ghostified", "am284"],
+["We are getting close to the true nature of Quantum. More to soon...", "ghostified", "am284"],
 ["Is NG+3 not the most influential mod of the pre-reality era? Of course the great Pelle would make a NG+3 reference!", "mod.ngp3", "am290"],
 
 //Replacement
