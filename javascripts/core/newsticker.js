cfe5692da0fcec78d853a7cc3f700ebb4dee35e8
aarextiaokhiao <aarextiaokhiao@gmail.com>
2026-09-11 22:47:12 -0400

    Update newsticker.js

--- javascripts/core/newsticker.js
+++ javascripts/core/newsticker.js
@@ -491,9 +491,9 @@ newsArray = [//always true
 ["In 1,752 AAD, the Nanofield solider ants declared war against Mattertopia's ant colony. One big crunch later, Mattertopia won the battle.", "hasAch('ng3p35')", "am137"],
 ["7/27 WHEN YOU SEE ANTIMATTER", true, "am195"],
 ["By then, Gamma Rays would pass through your body.", "PHOTON.unlocked() && ghSave.photons.amt.gte(1e10)", "am286"],
+["Does it even matters when nothing is matter?", true, "am208"],
 
 /* PLACEHOLDER
-["[placeholder]", false, "am208"],
 ["[placeholder]", false, "am232"],
 ["[placeholder]", false, "am256"],
 ["[placeholder]", false, "am271"],
