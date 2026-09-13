a3dc1fc9283627c903b037aa31056b43c8be0a4d
aarextiaokhiao <aarextiaokhiao@gmail.com>
2026-09-12 22:47:44 -0400

    Update newsticker.js

--- javascripts/core/newsticker.js
+++ javascripts/core/newsticker.js
@@ -492,9 +492,9 @@ newsArray = [//always true
 ["7/27 WHEN YOU SEE ANTIMATTER", true, "am195"],
 ["By then, Gamma Rays would pass through your body.", "PHOTON.unlocked() && ghSave.photons.amt.gte(1e10)", "am286"],
 ["Does it even matters when nothing is matter?", true, "am208"],
+["Does the perfect form of antimatter exist?", false, "am232"],
 
 /* PLACEHOLDER
-["[placeholder]", false, "am232"],
 ["[placeholder]", false, "am256"],
 ["[placeholder]", false, "am271"],
 ["[placeholder]", false, "am278"],
